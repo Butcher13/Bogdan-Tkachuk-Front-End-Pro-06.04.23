d0df56bfcd01255afd30a5aca7cbdbc4972dd315
Butcher13 <tkachuk.bogdan.0209@gmail.com>
2023-04-08 14:41:19 +0300

    add my home work to main.js

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-//var info=prompt("What is your name?");
-//alert("Hello, " + info + "!" + "How are you?");
-
-var a = prompt ("Введіть число від 1 до 10");
-var b = prompt ("Введіть число від 1 до 10");
-var c = parseInt(a) + parseInt(b);
-var g = parseInt(a) * parseInt(b);
-var h = parseInt(a) - parseInt(b);
-var j = parseInt(a) / parseInt(b);
-
-alert (c);
-alert (g);
-alert (h);
-alert (j);
+var hours = prompt('Скільки зараз годин?');
+var sec = hours * 60 * 60;
+alert(sec);
