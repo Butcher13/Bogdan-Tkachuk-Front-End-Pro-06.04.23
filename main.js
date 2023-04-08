9a8ef23fd1dedb0d2919441f1045bbdcabba964a
Butcher13 <tkachuk.bogdan.0209@gmail.com>
2023-04-08 14:24:56 +0300

    add * - /

--- main.js
+++ main.js
@@ -4,4 +4,11 @@
 var a = prompt ("Введіть число від 1 до 10");
 var b = prompt ("Введіть число від 1 до 10");
 var c = parseInt(a) + parseInt(b);
+var g = parseInt(a) * parseInt(b);
+var h = parseInt(a) - parseInt(b);
+var j = parseInt(a) / parseInt(b);
+
 alert (c);
+alert (g);
+alert (h);
+alert (j);
