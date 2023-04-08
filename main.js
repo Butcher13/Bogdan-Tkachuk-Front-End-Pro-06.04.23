214f40474e4a1f99e9fff816621055e721795526
Butcher13 <tkachuk.bogdan.0209@gmail.com>
2023-04-08 13:54:33 +0300

    add new branch four

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-var info=prompt("What is your name?");
-alert("Hello, " + info + "!" + "How are you?");
+//var info=prompt("What is your name?");
+//alert("Hello, " + info + "!" + "How are you?");
+
+var a = prompt ("Введіть число від 1 до 10");
+var b = prompt ("Введіть число від 1 до 10");
+var c = parseInt(a) + parseInt(b);
+alert (c);
